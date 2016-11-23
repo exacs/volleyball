c8ff4551a2a202171587927f26fd7c372cec16a2
Carlos Saito <c.saito@alumnos.upm.es>
2016-11-23 15:34:00 +0100

    Fix. Render Scoreboard component from server

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import express from 'express'
 import React from 'react'
 import { renderToString } from 'react-dom/server'
 
-import Hello from '../app/components/Hello'
+import Scoreboard from '../app/components/Scoreboard'
 
 function sendHTML (rootComponent, jsName) {
   return (`
@@ -34,7 +34,7 @@ function sendHTML (rootComponent, jsName) {
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send(sendHTML(<Hello name='world' />, 'index'))
+  res.send(sendHTML(<Scoreboard />, 'index'))
 })
 
 export default app
