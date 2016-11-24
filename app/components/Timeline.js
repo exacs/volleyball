00388a19f40343c338846f6bda32e78b06e87a77
Carlos Saito <c.saito@alumnos.upm.es>
2016-11-24 12:16:16 +0100

    Lift state from TimelineEntry to Timeline

--- app/components/Timeline.js
+++ app/components/Timeline.js
@@ -4,13 +4,17 @@ import TimelineEntry from './TimelineEntry'
 const Timeline = () => (
   <div className='timeline'>
     <ul className='timeline--list'>
-    {
-      [1, 2].map(i =>
-        <li className='timeline--item'>
-          <TimelineEntry key={i} />
-        </li>
-      )
-    }
+      <li className='timeline--item'>
+        <TimelineEntry time={60} points={{home: 15, away: 4}} home />
+      </li>
+
+      <li className='timeline--item'>
+        <TimelineEntry time={50} points={{home: 15, away: 3}} />
+      </li>
+
+      <li className='timeline--item'>
+        <TimelineEntry time={48} points={{home: 15, away: 2}} />
+      </li>
     </ul>
   </div>
 )
