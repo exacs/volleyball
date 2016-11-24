dcf71569f49bb01d242572860df57a118b2b571b
Carlos Saito <c.saito@alumnos.upm.es>
2016-11-24 12:31:54 +0100

    Lift state from Scoreboard to SpectatorRoot

--- app/SpectatorRoot.js
+++ app/SpectatorRoot.js
@@ -2,9 +2,20 @@ import React from 'react'
 import Scoreboard from './components/Scoreboard'
 import Timeline from './components/Timeline'
 
+const teams = {
+  home: {
+    shortName: 'UAH',
+    name: 'Alcalá'
+  },
+  away: {
+    shortName: 'UAM',
+    name: 'Autónoma'
+  }
+}
+
 const SpectatorRoot = () => (
   <div>
-    <Scoreboard />
+    <Scoreboard round={3} teams={teams} points={{ home: 1, away: 4 }} />
     <Timeline />
   </div>
 )
--- app/components/Scoreboard.js
+++ app/components/Scoreboard.js
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import TeamScore from './TeamScore'
 
-const Scoreboard = () => (
+const Scoreboard = ({ round, teams, points }) => (
   <div className='score-board pb-3'>
-    <h2 className='score-board--title pb-1 pt-1'>First set</h2>
+    <h2 className='score-board--title pb-1 pt-1'>SET {round}</h2>
     <div className='score-board--teams'>
       <div className='score-board--team score-board__local'>
-        <TeamScore shortName='UPM' name='Politécnica' points={15} local />
+        <TeamScore shortName={teams.home.shortName} name={teams.home.name} points={points.home} local />
       </div>
       <div className='score-board--team score-board__visitor'>
-        <TeamScore shortName='UCM' name='Complutense' points={4} />
+        <TeamScore shortName={teams.away.shortName} name={teams.away.name} points={points.away} />
       </div>
     </div>
   </div>
 )
+
+const teamShape = {
+  shortName: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired
+}
+
+Scoreboard.propTypes = {
+  round: PropTypes.number.isRequired,
+  teams: PropTypes.shape({
+    home: PropTypes.shape(teamShape),
+    away: PropTypes.shape(teamShape)
+  }),
+  points: PropTypes.shape({
+    home: PropTypes.number.isRequired,
+    away: PropTypes.number.isRequired
+  })
+}
 
 export default Scoreboard
