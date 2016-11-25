d75c90c9dff06109d76ee5b2bd1260892d2dbe0d
Carlos Saito <c.saito@alumnos.upm.es>
2016-11-25 11:49:26 +0100

    Add JS compression

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -27,6 +27,11 @@ module.exports = {
   plugins: base.plugins.concat([
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: true
+      }
     })
   ])
 }
