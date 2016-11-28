bc95d89c6f78c90c476285d7d4536c53c559874c
Carlos Saito <c.saito@alumnos.upm.es>
2016-11-28 10:17:36 +0100

    Insert caret icons in TimelineEntry

--- app/components/TimelineEntry.js
+++ app/components/TimelineEntry.js
@@ -3,15 +3,17 @@ import React, { PropTypes } from 'react'
 const TimelineEntry = ({home = false, time, points}) => (
   <div className='timeline-entry'>
     <time className='timeline-entry--node'>
-      <span className='timeline-entry--time'>{ time + '\'' }</span>
+      <span className='timeline-entry--time'>{ 'X\'' }</span>
     </time>
 
-    <div className={`timeline-entry--label timeline-entry--label__home${home ? '__active' : ''}`}>
+    <div className='timeline-entry--label timeline-entry--label__home'>
+      { home && <i className='material-icons'>keyboard_arrow_right</i> }
       <header className='timeline-entry--title'>Home</header>
       <main className='timeline-entry--points'>{ points.home }</main>
     </div>
 
-    <div className={`timeline-entry--label timeline-entry--label__away${home ? '' : '__active'}`}>
+    <div className='timeline-entry--label timeline-entry--label__away'>
+      { !home && <i className='material-icons'>keyboard_arrow_left</i> }
       <header className='timeline-entry--title'>Away</header>
       <main className='timeline-entry--points'>{ points.away }</main>
     </div>
