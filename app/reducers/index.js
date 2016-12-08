9416da92469479b3a454200d05a3d66f03a13d8b
Carlos Saito <c.saito.style@gmail.com>
2016-12-08 20:43:10 +0100

    Logic of increment round when finishes

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -5,14 +5,37 @@ function reducer (state, action) {
     case ActionTypes.POINT:
       return pointReducer(state, action)
     case ActionTypes.UNDO:
-      return undoReducer(state, action)
+      return Object.assign(
+        { history: state.history },
+        undoReducer(state, action)
+      )
     default:
       return state
   }
 }
 
+function winRound (state) {
+  const diff = Math.abs(state.points.home - state.points.away)
+  if (diff >= 2 && (state.points.home >= 25 || state.points.away >= 25)) {
+    return {
+      points: {
+        home: 0,
+        away: 0
+      },
+      rounds: {
+        home: state.rounds.home + (state.points.home > state.points.away),
+        away: state.rounds.away + (state.points.away > state.points.home)
+      },
+      history: []
+    }
+  } else {
+    return state
+  }
+}
+
 function pointReducer (state, action) {
-  return {
+  return winRound({
+    rounds: state.rounds,
     points: {
       home: state.points.home + (action.feature === 'home'),
       away: state.points.away + (action.feature === 'away')
@@ -22,7 +45,7 @@ function pointReducer (state, action) {
       action: 'point',
       feature: action.feature
     })
-  }
+  })
 }
 
 function undoReducer (state) {
