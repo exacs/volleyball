7091a98a85355972f02a69e0b7c060a0b0aa53f3
Carlos Saito <c.saito.style@gmail.com>
2016-12-13 05:21:55 +0100

    Showcase with all Spectator components

--- client/showcase.js
+++ client/showcase.js
@@ -6,6 +6,9 @@ import React from 'react'
 import { render } from 'react-dom'
 
 import TimelineEntry from '../app/components/TimelineEntry'
+import Timeline from '../app/components/Timeline'
+import TeamScore from '../app/components/TeamScore'
+import Scoreboard from '../app/components/Scoreboard'
 
 render(
   <TimelineEntry
@@ -13,4 +16,36 @@ render(
     points={{ home: 0, away: 0 }}
     feature='home'/>,
   document.getElementById('timeline-entry')
+)
+
+render(
+  <Timeline
+    undo={() => {}}
+    history={[
+      { time: 0, action: 'point', feature: 'home' },
+      { time: 0, action: 'point', feature: 'home' }
+    ]} />,
+  document.getElementById('timeline')
+)
+
+render(
+  <TeamScore
+    name='local'
+    shortName='L'
+    points={6}
+    feature='home' />,
+  document.getElementById('team-score')
+)
+
+render(
+  <Scoreboard
+    rounds={[
+      { home: 25, away: 21, winner: 'home' },
+      { home: 23, away: 24, winner: null }
+    ]}
+    teams={{
+      home: { name: 'home', shortName: 'L' },
+      away: { name: 'away', shortName: 'V' }
+    }} />,
+  document.getElementById('team-score')
 )
